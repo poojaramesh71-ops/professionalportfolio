336682d82c80ffc335a38c18afed0fda7acfd4ff
Pooja Ramesh <kumbarp71@gmail.com>
2026-03-11 19:28:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const menu = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
-menu.addEventListener('click', () => {
+menu.onclick = () => {
 menu.classList.toggle('bx-x');
 navbar.classList.toggle('active');
-});
+};
 
-const typed = new Typed('.multiple-text', {
+new Typed('.multiple-text', {
 strings: [
 'Frontend Developer',
 'Backend Developer',
 'Web Developer'
 ],
-typeSpeed:80,
-backSpeed:80,
-backDelay:1200,
-loop:true
+typeSpeed: 80,
+backSpeed: 80,
+backDelay: 1200,
+loop: true
 });
+
