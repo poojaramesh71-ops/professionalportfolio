fc47042d4788ef2de3928ad3e32d9e1b8a28e706
Pooja Ramesh <kumbarp71@gmail.com>
2026-06-18 08:35:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,17 @@ let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
 menu.classList.toggle('bx-x');
 navbar.classList.toggle('active');
-};
+}
 
-document.querySelectorAll('.navbar a').forEach(link => {
-link.addEventListener('click', () => {
+window.onscroll = () => {
 menu.classList.remove('bx-x');
 navbar.classList.remove('active');
-});
-});
+}
 
-new Typed('.multiple-text', {
-strings: [
-'Frontend Developer',
-'Backend Developer',
-'Web Developer'
-],
-typeSpeed: 80,
-backSpeed: 80,
-backDelay: 1200,
-loop: true
+const typed = new Typed('.multiple-text', {
+  strings: ['Frontend Developer', 'Backend Developer', 'Web Developer'],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 1200,
+  loop: true,
 });
